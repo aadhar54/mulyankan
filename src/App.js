e7740e3523f7c931fffece45aab822498f8e5def
Soham K <sohamk10@gmail.com>
2020-06-09 21:30:09 +0530

    started

--- src/App.js
+++ src/App.js
@@ -7,33 +7,6 @@ const App = () => {
   const [pdf, setPdf] = useState(null);
   const [scale, setScale] = useState(1);
   const [urls, setUrls] = useState([]);
-  const [startRender, setStartRender] = useState(false);
-
-  useEffect(() => {
-    if (pdf) {
-      [...Array(pdf.data.numPages).keys()].forEach(async (index) => {
-        const pageNumber = index + 1;
-        const page = await pdf.data.getPage(pageNumber);
-        console.log('getting page ' + pageNumber);
-        let viewport = page.getViewport({ scale: 1 });
-        let canvas = document.createElement('canvas');
-        let ctx = canvas.getContext('2d');
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-        const renderContext = {
-          canvasContext: ctx,
-          viewport,
-        };
-        const task = page.render(renderContext);
-        task.promise.then(() => {
-          let d = canvas.toDataURL();
-          setUrls((prevUrls) => prevUrls.concat(d));
-        });
-
-        return <h1>Hello World</h1>;
-      });
-    }
-  }, [pdf]);
 
   return !pdf ? (
     <SelectPDF setPdf={setPdf} />
@@ -41,8 +14,10 @@ const App = () => {
     <main>
       <div className="main-container">
         <Sidebar title={pdf.name} scale={scale} setScale={setScale} />
-        {pdf.data.numPages === urls.length
-          ? urls.map((url, index) => <Cv key={index} index={index} url={url} />)
+        {pdf
+          ? [...Array(pdf.data.numPages).keys()].map((pg) => (
+              <Cv key={pg} pg={pg + 1} pdf={pdf.data} />
+            ))
           : null}
       </div>
     </main>
--- src/Cv.js
+++ src/Cv.js
@@ -1,20 +1,26 @@
 import React from 'react';
 const fabric = require('fabric').fabric;
 
-const Cv = ({ url, index }) => {
-  const init = () => {
-    const canvas = new fabric.Canvas(`page-${index}`);
-    const img = new Image();
-    img.onload = () => {
-      const fimg = new fabric.Image(img);
-      canvas.setOverlayImage(url, canvas.renderAll.bind(canvas));
-    };
+const Cv = ({ pdf, pg }) => {
+  const init = async () => {
+    const canvas = document.getElementById();
+    const canvasContext = canvas.getContext('2d');
+    pdf.getPage(pg).then((page) => {
+      const viewport = page.viewport({ scale: 1 });
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+      const task = page.render({ canvasContext, viewport });
+      task.promise.then(() => {
+        const bg = canvas.toDataURL();
+        console.log(bg);
+      });
+    });
   };
   init();
 
   return (
     <div className="canvas-container">
-      <canvas className={`page-${index}`} id={`page-${index}`}></canvas>
+      <canvas className={`page-${pg}`} id={`page-${pg}`}></canvas>
     </div>
   );
 };
