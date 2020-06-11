577d19bcdbaad1959ab3a3a28f95a0ce3e00e817
Soham K <sohamk10@gmail.com>
2020-06-12 00:10:53 +0530

    FIxed emoji

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -163,7 +163,13 @@ const Sidebar = ({ title, logURLs, setZoom, editText }) => {
       </div>
       <div className="divider"></div>
       <div className="copyright">
-        <div className="copyright-1">Made with ❤️ by Soham Karandikar.</div>
+        <div className="copyright-1">
+          Made with{' '}
+          <span role="img" aria-label="love">
+            ❤️
+          </span>{' '}
+          by Soham Karandikar.
+        </div>
         <div className="copyright-2">
           &copy; 2020 by Soham Karandikar. All rights reserved.
         </div>
